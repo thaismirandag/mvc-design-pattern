9fd39e4f408a13f1481cd8784507b81324b89355
thais <thais.miranda.g@gmail.com>
2024-06-11 19:50:10 -0300

    :sparkles: feat: create schema database (SQLite)

---
+++ init/schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS 'pets' (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255) NOT NULL,
+    type VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS 'people' (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    age INTEGER NOT NULL,
+    pet_id INTEGER NOT NULL,
+    FOREIGN KEY (pet_id) REFERENCES pets(id)
+);
+
+INSERT INTO pets (name, type) VALUES ('cobra', 'snake'), ('cao', 'dog'), ('gato', 'cat');
