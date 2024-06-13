6d382a5667f387d8e17fdd52726a8166456187d6
Thais Gomes <thaismrgs@gmail.com>
2024-06-12 21:51:27 -0300

    :sparkles: feat: create interfaces in controllers

--- src/controllers/person_creator_controller.py
+++ src/controllers/person_creator_controller.py
@@ -1,7 +1,6 @@
 import re
 from src.models.sqlite.interfaces.people_repository import PeopleRepositoryInterface
 
-
 class PersonCreatorController:
     def __init__(self, people_repository: PeopleRepositoryInterface) -> None:
         self.__people_repository = people_repository
